b9bd19f85d3bb320558f4b356bbc52486409ed6f
Lucas Cassilha <lucaszawadneak@gmail.com>
2020-04-22 10:11:31 -0300

    CSS fix

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;1,700;1,900&display=swap');
-
   *{
     margin:0;
     padding: 0;
