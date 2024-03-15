ebe227c097e4bfee4f2a2a4798883f79b1ee38bc
Lucas Zawadneak <lucaszawadneak@gmail.com>
2024-03-15 17:15:27 -0300

    Update useProjects.js

--- hooks/useProjects.js
+++ hooks/useProjects.js
@@ -18,7 +18,7 @@ export default function useProjects() {
       tech: "React | Supabase",
       year: "2023",
       description: t("projects.just-poker.description"),
-      image: "https://curistudios.com/_astro/just-poker_Uurmh.avif",
+      image: "https://justpoker.curistudios.com/assets/assets/branding/just-poker.png",
       web: "https://justpoker.curistudios.com/",
     },
     // {
@@ -36,7 +36,7 @@ export default function useProjects() {
       year: "2019",
       description: t("projects.maruska.description"),
       image:
-        "https://camo.githubusercontent.com/a56e38f835f0ac61df5a1659abe2d9c3de2cff1a53a690993808fe461a146308/68747470733a2f2f6c68332e676f6f676c6575736572636f6e74656e742e636f6d2f595a4e456c50516b6e38634f44563935526c665f75414976736832614c4a304670306b456266685537337632324d337342745364456357644659487731553842554e5632",
+        "https://camo.githubusercontent.com/0dce506ed8b52eb6129ed45e4ce1ca3f78f4a689704a9bf5eb38f41856c5ba99/68747470733a2f2f6c68332e676f6f676c6575736572636f6e74656e742e636f6d2f595a4e456c50516b6e38634f44563935526c665f75414976736832614c4a304670306b456266685537337632324d337342745364456357644659487731553842554e5632",
       github: "https://github.com/lucaszawadneak/maruska",
     },
   ];
