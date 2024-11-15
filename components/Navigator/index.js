dcd5837b837a118a89b5538bcce2f8cf541dfa89
Zawadneak <lucaszawadneak@gmail.com>
2024-11-15 14:31:21 -0300

    Adjust navbar

--- components/Navigator/index.js
+++ components/Navigator/index.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { IoHomeOutline, IoLeafOutline } from "react-icons/io5";
+import { IoHomeOutline, IoHomeSharp, IoLeafOutline } from "react-icons/io5";
 
 import { motion } from "framer-motion";
 import { useTranslation } from "next-i18next";
@@ -27,21 +27,11 @@ export default function Navigator() {
   }, []);
 
   return (
-    <nav className="flex flex-col items-center justify-center w-full pt-10">
-      <div className="flex w-full max-w-6xl px-20 items-center justify-center lg:justify-end">
-        <Link href="/" legacyBehavior>
-          <motion.a
-            className="mr-4"
-            transition={{ ease: "anticipate", duration: 1 }}
-            animate={{
-              opacity: router.pathname !== "/" ? 1 : 0,
-              display: router.pathname !== "/" ? "flex" : "hidden",
-              cursor: router.pathname !== "/" ? "pointer" : "default",
-            }}
-          >
-            <IoHomeOutline size={25} />
-          </motion.a>
-        </Link>
+    <nav className="flex flex-row items-center justify-between w-full p-8  md:p-10 md:px-40">
+      <Link href="/">
+        <IoHomeSharp size={24} />
+      </Link>
+      <div className="flex  items-center justify-center lg:justify-end">
         <Link legacyBehavior href="/education">
           <a className="mr-4 cursor-pointer font-semibold">{t("education")}</a>
         </Link>
@@ -53,16 +43,8 @@ export default function Navigator() {
         <Link legacyBehavior href="/contact">
           <a className=" cursor-pointer flex font-semibold">{t("contact")}</a>
         </Link>
-        {/* <a
-          href="https://curistudios.com"
-          className="ml-4  cursor-pointer font-semibold"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          curi
-        </a> */}
-        <button onClick={handleChangeLanguage} className="ml-4 mt-2">
-          {language === "en" ? (
+        <button onClick={handleChangeLanguage} className="ml-4 min-w-[20px]">
+          {language === "pt" ? (
             <Image src={UsaFlag} width={25} height={25} />
           ) : (
             <Image src={BrazilFlag} width={25} height={25} />
